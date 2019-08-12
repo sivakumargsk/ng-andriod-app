a9a56fa173b49e561dffae8f3c6417d0b930fef5
sivakumar.gsk <geddasiva777@gmail.com>
2019-08-12 18:43:48 +0530

    angular app ->  cordova based andriod app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-andriod-app';
+
+  ngOnInit() {
+    document.addEventListener(
+      'deviceready',
+      () => {
+        alert(device.platform);
+      },
+      false
+    );
+  }
 }
